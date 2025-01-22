bf60cdd0b7cff5963c5048cfa256c1f3417701af
shanukaumesh <shanukaumesh0@gmail.com>
2025-01-22 10:55:55 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+
+            for(int j=10; j>i;j--)
+            {
+                System.out.println("*");
+            }
+
+        }
+
+
+    }
+
+}
