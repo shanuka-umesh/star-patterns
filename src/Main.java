32ec2506fd504a02641daf5a230efbcdefecc197
shanukaumesh <shanukaumesh0@gmail.com>
2025-01-22 12:38:00 +0530

    Created second star pattern

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-
-            for(int j=10; j>i;j--)
-            {
-                System.out.println("*");
-            }
-
-        }
+       //starPattern01();
+         reverseStarPattern01();
 
 
     }
 
+
+    public static void starPattern01(){
+
+        int lineCount = 5;
+
+        for (int i = 1; i <= lineCount; i++) {
+
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println("");
+        }
+
+    }
+
+    public static void reverseStarPattern01(){
+
+        int linecount =5;
+
+        for(int i=linecount; i > 0 ; i--)
+        {
+            for(int j=0 ; j<i ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
 }
